eab650e413f40a5e0b324e024a507d7850d9bf9a
mirkoc94 <mirkocuciuffo@hotmail.it>
2024-02-08 22:08:23 +0100

    tombola

--- m1/s3/g4/js/script.js
+++ m1/s3/g4/js/script.js
@@ -1,27 +1,38 @@
 //elementi generali
 
-const caselleElement = document.querySelector('#caselle')
+const tabelloneElement = document.querySelector('#tabellone')
 const button = document.querySelector('#btn')
 const randomElement = document.querySelector('.random')
 
 
 //bottone
 
-button.addEventListener('click', () => {
-    selectNumber()
-})
-
-function selectNumber() {
-    randomElement.innerHTML = Math.floor(Math.random() * 90) + 1
-}
+button.addEventListener('click', () => selectNumber())
 
 
 //caselle numeri
 
-for(let i = 1;i <= 90; i++ ){
+let n = 90
+let arrNumb = []
+
+for(let i = 1; i <= n; i++ ) {
     const numbBox = document.createElement('div')
     numbBox.classList.add('number')
     numbBox.innerText = i
+    
+    arrNumb[i] = numbBox
+    
+    tabelloneElement.append(numbBox)
+}
 
-    caselleElement.append(numbBox)
-    }
+let sacchetto = [...arrNumb]
+
+function evidenzia() {
+    let estratto = selectNumber()
+    sacchetto.splice(estratto, 1)
+    arrNumb[estratto].classList.add('selectedNumber')
+}
+
+function selectNumber() {
+    randomElement.innerHTML = Math.floor(Math.random() * 90) + 1
+}
