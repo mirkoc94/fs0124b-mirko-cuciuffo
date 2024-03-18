f1396921a4249c6bbd89c130a5f43a46ad39297d
mirkoc94 <mirkocuciuffo@hotmail.it>
2024-03-18 16:55:27 +0100

    fine esercizio g1

---
+++ m3/s1/g1/index.ts
@@ -0,0 +1,47 @@
+interface iSmartphone{
+    credito:number
+    numeroChiamate:number
+    ricarica(ammontare:number): number
+    chiamata(durataChiamata:number|string): (number|string)
+    get chiama404(): number
+    get getNumeroChiamata(): number
+    azzeraChiamate(numeroChiamate:number): number
+}
+
+class User implements iSmartphone{
+    nome:string
+    cognome:string
+    constructor(name:string, lastName:string) {
+        this.nome = name
+        this.cognome = lastName
+    }
+    credito:number = 0
+    numeroChiamate:number = 0
+    ricarica (ammontare:number): number {
+        return ammontare+=this.credito
+    }
+    chiamata(durataChiamata: number|string):(number|string) {
+        let creditoRimanente = (this.credito -= (durataChiamata*0.20))
+        if (creditoRimanente<0) {
+            return 'Credito insufficiente'
+        }
+        return creditoRimanente
+    }
+    get chiama404(): number {
+        return this.credito
+    }
+    get getNumeroChiamata(): number {
+        return this.numeroChiamate
+    }
+    azzeraChiamate(numeroChiamate: number): number {
+        return numeroChiamate=0
+    }
+}
+
+const mario = new User('mario','rossi')
+console.log(mario.chiama404);
+console.log(mario.getNumeroChiamata);
+
+
+
+
