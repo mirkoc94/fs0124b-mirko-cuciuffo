0ba67968d809b41d32de83113bcfc58da58d4af6
mirkoc94 <mirkocuciuffo@hotmail.it>
2024-03-20 16:59:17 +0100

    boh

---
+++ m3/s1/g3/esercizioG3/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { iPost } from './Models/post';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'esercizioG3';
+}
