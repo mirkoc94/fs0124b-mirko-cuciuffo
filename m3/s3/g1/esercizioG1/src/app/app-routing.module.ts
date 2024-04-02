0c031714a8df36e10b0638755247ad3df97452f6
mirkoc94 <mirkocuciuffo@hotmail.it>
2024-04-02 23:55:13 +0200

    shared e guard

--- m3/s3/g1/esercizioG1/src/app/app-routing.module.ts
+++ m3/s3/g1/esercizioG1/src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './Pages/auth/auth.guard';
 
 const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./Pages/home/home.module')
-    .then(m => m.HomeModule)
-  }
+    .then(m => m.HomeModule),
+    canActivate: [AuthGuard],
+    canActivateChild: [AuthGuard]
+  },
+  { path: 'auth', loadChildren: () => import('./Pages/auth/auth.module').then(m => m.AuthModule) }
 ];
 
 @NgModule({
