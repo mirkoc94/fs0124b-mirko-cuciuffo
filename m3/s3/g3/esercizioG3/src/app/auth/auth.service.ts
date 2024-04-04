2a34489978173abfe74658b1f9d32a13400809d5
mirkoc94 <mirkocuciuffo@hotmail.it>
2024-04-04 16:59:41 +0200

    pre login interface

--- m3/s3/g3/esercizioG3/src/app/auth/auth.service.ts
+++ m3/s3/g3/esercizioG3/src/app/auth/auth.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
+import { iUser } from '../models/i-user';
+import { Observable, map } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+type AccessData = {
+  accessToken:string
+  user:iUser
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  jwtHelper:JwtHelperService = new JwtHelperService()
+
+  authSubject = new BehaviorSubject<iUser|null>(null)
+
+  user$ = this.authSubject.asObservable()
+  isLoggedIn$ = this.user$.pipe(map(user => !!user))
+
+  constructor(private http:HttpClient) { }
+
+  register(newUser:Partial<iUser>):Observable<AccessData>{
+    return this.http.post<AccessData>('http://localhost:3000/register',newUser)
+  }
+
+  //login(loginData:any):Observable<AccessData>{
+//
+  //}
+
 }
---
+++ m3/s3/g3/esercizioG3/src/app/models/i-user.ts
@@ -0,0 +1,6 @@
+export interface iUser {
+  id: number
+  name: string
+  email: string
+  password: string
+}
